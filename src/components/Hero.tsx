1c833621917adad4a7dbf9dee17df52d11bfbec8
Hemali Firke <hemali.firke@gmail.com>
2026-04-09 19:31:38 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,8 @@ export default function Hero() {
 
           {/* Profile Image */}
           <div className="mb-6 mt-20 relative">
-  <div className="w-60 h-60 mx-auto rounded-full bg-gradient-to-r from-blue-500 to-emerald-400 p-[3px] shadow-[0_0_35px_rgba(0,255,255,0.6)]">
+  className="w-60 h-60 mx-auto rounded-full bg-gradient-to-r from-blue-500 to-emerald-400 p-[3px] 
+shadow-[0_0_25px_rgba(0,255,255,0.8),0_0_60px_rgba(0,255,255,0.6),0_0_90px_rgba(0,255,255,0.4)] animate-pulse"
 
     <img
       src="/profile-photo.png"
